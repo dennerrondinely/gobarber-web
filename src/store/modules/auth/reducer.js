6658a365ca93f4e8c42da9e22072603326023d82
Denner Rondinely <denner.rondinely@gmail.com>
2020-05-20 20:26:55 -0300

    Criar ssitema de login

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,6 +1,19 @@
-const INICITAL_STATE = {};
+import produce from 'immer';
+
+const INICITAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
+
 export default function auth(state = INICITAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
+
     default:
       return state;
   }
