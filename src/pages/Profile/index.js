8840048734a865dc1493756b098324e96f4f511a
Denner Rondinely <dennerrondinely@DESKTOP-TQ8EP5E.localdomain>
2020-05-26 22:12:09 -0300

    Criar sistema de alterar perfil do usuário

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
+
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import AvatarInput from './AvatarInput';
 
 import { Container } from './styles';
 
 export default function Profile() {
   const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" placeholder="Nome completo" />
         <Input name="email" type="email" placeholder="Seu email" />
         <hr />
@@ -20,10 +29,10 @@ export default function Profile() {
           name="oldPassword"
           placeholder="Sua senha atual"
         />
-        <Input type="password" name="newPassword" placeholder="Nova senha" />
+        <Input type="password" name="password" placeholder="Nova senha" />
         <Input
           type="password"
-          name="newPassword"
+          name="confirmPassword"
           placeholder="Confirmação de senha"
         />
         <button type="submit">Atualizar perfil</button>
--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -3,13 +3,18 @@ import produce from 'immer';
 const INICITAL_STATE = { profile: null };
 
 export default function user(state = INICITAL_STATE, action) {
-  switch (action.type) {
-    case '@auth/SIGN_IN_SUCCESS':
-      return produce(state, draft => {
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@auth/SIGN_IN_SUCCESS': {
         draft.profile = action.payload.user;
-      });
+        break;
+      }
+      case '@user/UPDATE_PROFILE_SUCCESS': {
+        draft.profile = action.payload.profile;
+        break;
+      }
 
-    default:
-      return state;
-  }
+      default:
+    }
+  });
 }
